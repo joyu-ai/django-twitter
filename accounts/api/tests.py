0f38736e9e7953a401d5dd27e9eece44a4a7c172
Jonelle Yu <joyu@MacBook-Pro.local>
2022-11-08 16:16:59 -0700

    22-userprofile-model-and-admin

--- accounts/api/tests.py
+++ accounts/api/tests.py
@@ -1,6 +1,7 @@
-from testing.testcases import TestCase
-from rest_framework.test import APIClient
+from accounts.models import UserProfile
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from testing.testcases import TestCase
 
 
 LOGIN_URL = '/api/accounts/login/'
@@ -126,6 +127,10 @@ class AccountApiTests(TestCase):
         response = self.client.post(SIGNUP_URL, data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['user']['username'], 'someone')
+        # 验证 user profile 已经被创建
+        created_user_id = response.data['user']['id']
+        profile = UserProfile.objects.filter(user_id=created_user_id).first()
+        self.assertNotEqual(profile, None)
         # 验证用户已经登入
         response = self.client.get(LOGIN_STATUS_URL)
         self.assertEqual(response.data['has_logged_in'], True)
--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class TweetsConfig(AppConfig):
+class AccountsConfig(AppConfig):
     name = 'accounts'
