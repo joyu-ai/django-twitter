f956f5248948c03d8538d7ffee53ed010342a8ad
Jonelle Yu <joyu@MacBook-Pro.local>
2022-11-11 16:26:08 -0700

    24-photos-model-admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,6 +1,6 @@
 # 这个admin就是Django自己的admin
 from django.contrib import admin
-from tweets.models import Tweet
+from tweets.models import Tweet, TweetPhoto
 
 
 @admin.register(Tweet) # decorator
@@ -11,3 +11,17 @@ class TweetAdmin(admin.ModelAdmin):
         'user',
         'content',
     )
+
+
+@admin.register(TweetPhoto)
+class TweetPhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'tweet',
+        'user',
+        'file',
+        'status',
+        'has_deleted',
+        'created_at',
+    )
+    list_filter = ('status', 'has_deleted')
+    date_hierarchy = 'created_at'
