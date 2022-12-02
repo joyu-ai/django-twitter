a001ca7d504e53fb74e1667210d9b1613009d5c5
Jonelle Yu <joyu@MacBook-Pro.local>
2022-12-01 18:53:22 -0700

    26-friendships-pagination

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,27 +1,50 @@
 from accounts.api.serializers import UserSerializerForFriendship
+from django.contrib.auth.models import User
 from friendships.models import Friendship
+from friendships.services import FriendshipService
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from django.contrib.auth.models import User
+
 
 # 可以通过 source=xxx 指定去访问每个 model instance 的 xxx 方法
 # 即 model_instance.xxx 来获得数据
 # https://www.django-rest-framework.org/api-guide/serializers/#specifying-fields-explicitly
 class FollowerSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='from_user')
-    # created_at = serializers.DateTimeField() # 不加网页也可以显示created_at
+    created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
 
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
+        # 我们将在后序的课程中解决这个问题
+        return FriendshipService.has_followed(self.context['request'].user,
+                                              obj.from_user
+                                              )
 
 class FollowingSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='to_user')
+    created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
+        # 我们将在后序的课程中解决这个问题
+        return FriendshipService.has_followed(self.context['request'].user,
+                                              obj.to_user
+                                              )
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
     from_user_id = serializers.IntegerField()
