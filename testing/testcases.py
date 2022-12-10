ec8568dcc22432620185202e29272ce12b475730
Jonelle Yu <joyu@MacBook-Pro.local>
2022-12-10 09:10:14 -0700

    32-redis-as-cache

--- testing/testcases.py
+++ testing/testcases.py
@@ -7,12 +7,14 @@ from likes.models import Like
 from newsfeeds.models import NewsFeed
 from rest_framework.test import APIClient
 from tweets.models import Tweet
+from utils.redis_client import RedisClient
 
 
 class TestCase(DjangoTestCase):
 
     def clear_cache(self):
         caches['testing'].clear()
+        RedisClient.clear()
 
     @property
     def anonymous_client(self):
