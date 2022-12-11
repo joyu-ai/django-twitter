a82130d9ee38c8904158a48e3933bad3b0acfd7d
Jonelle Yu <joyu@MacBook-Pro.local>
2022-12-11 12:27:54 -0700

    34-cache-newsfeed-list-in-redis

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -1,5 +1,5 @@
 from newsfeeds.api.serializers import NewsFeedSerializer
-from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from utils.paginations import EndlessPagination
@@ -9,18 +9,19 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     pagination_class = EndlessPagination
 
-    def get_queryset(self):
-        # 自定义 queryset, 因为 newsfeed 的查看是有权限的
-        # 只能看 user=当前登录用户的 newsfeed
-        # 也可以是 self.request.user.newsfeed_set.all()
-        # 但是一般最好还是按照 NewsFeed.objects.filter 的方式写，更清晰直观
-        return NewsFeed.objects.filter(user=self.request.user)
+    # def get_queryset(self):
+    #     # 自定义 queryset, 因为 newsfeed 的查看是有权限的
+    #     # 只能看 user=当前登录用户的 newsfeed
+    #     # 也可以是 self.request.user.newsfeed_set.all()
+    #     # 但是一般最好还是按照 NewsFeed.objects.filter 的方式写，更清晰直观
+    #     return NewsFeed.objects.filter(user=self.request.user)
 
     def list(self, request):
-        queryset = self.paginate_queryset(self.get_queryset())
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(request.user.id)
+        page = self.paginate_queryset(newsfeeds)
         serializer = NewsFeedSerializer(
-            queryset,
-            context={'request': request}, # 20.4 TweetSerial 扩展，被 NewsFeedSerializer 调用
+            page,
+            context={'request': request},
             many=True,
         )
         return self.get_paginated_response(serializer.data)
